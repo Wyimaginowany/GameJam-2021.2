9c5410cc7da4ad5f14ccc025c2813f7183459e67
BaseWarsOfficial <goralczyk.jakub@wp.pl>
2021-08-23 11:36:34 +0200

    player shooting

---
+++ GameJam 2001.2/Assets/PlayerShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 30f;
+
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bulletPrefab;
+
+    [SerializeField] float fireRate = 5f;
+    float lastFired = 0f;
+
+
+    public void Shoot()
+    {
+        if (Time.time - lastFired > 1 / fireRate)
+        {
+            lastFired = Time.time;
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+            bulletRigidbody.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+        }     
+    }
+
+}
