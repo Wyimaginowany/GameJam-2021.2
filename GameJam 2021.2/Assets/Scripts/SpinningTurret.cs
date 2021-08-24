d8f146cd89f08847f39308b81947f4182b4c3220
BaseWarsOfficial <goralczyk.jakub@wp.pl>
2021-08-24 19:16:22 +0200

    traps added
    3 traps added

---
+++ GameJam 2021.2/Assets/Scripts/SpinningTurret.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningTurret : MonoBehaviour
+{
+    [SerializeField] float spiningSpeed = 200f;
+    [SerializeField] float fireRate;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float damage;
+    [SerializeField] GameObject movingParts;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+
+    float lastFired = 0f;
+
+    private void Update()
+    {
+        movingParts.transform.Rotate(new Vector3(0f, 0f, spiningSpeed) * Time.deltaTime);
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (Time.time - lastFired > 1 / fireRate)
+        {
+            lastFired = Time.time;
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.transform.position, firePoint.transform.rotation);
+            bullet.GetComponent<Bullet>().SetBulletDamage(damage);
+            Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+            bulletRigidbody.AddForce(firePoint.transform.up * bulletSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
