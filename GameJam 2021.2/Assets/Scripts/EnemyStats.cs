5d2b0ed60dc314c46a53188dfbe08ba2ac9f7ec4
BaseWarsOfficial <goralczyk.jakub@wp.pl>
2021-08-28 12:27:34 +0200

    player health

--- GameJam 2021.2/Assets/Scripts/EnemyStats.cs
+++ GameJam 2021.2/Assets/Scripts/EnemyStats.cs
@@ -44,4 +44,5 @@ public class EnemyStats : MonoBehaviour
         return isAlive;
     }
 
+
 }
---
+++ GameJam 2021.2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float invincibleDuration = 1f;
+
+
+    bool invincible = false;
+    int playerHealth = 5;
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy") && !invincible)
+        {
+            TakeDamege();
+        }
+    }
+
+
+    public void TakeDamege()
+    {
+        playerHealth--;
+        invincible = true;
+        if (playerHealth <= 0)
+        {
+            HandleDeath();
+        }
+        else
+        {
+            Invoke("ActiveCollider", invincibleDuration);
+            //make it so the player can see that he is invincible
+            //turn off collider for invincibleDuration
+        }
+    }
+
+    private void HandleDeath()
+    {
+        //destroy player
+        //show death ui
+        //stop all enemies and animations
+        Debug.Log("player died");
+    }
+
+    private void ActiveCollider()
+    {
+        invincible = false;
+    }
+
+
+
+}
