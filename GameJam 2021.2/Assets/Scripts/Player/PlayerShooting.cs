8323d648dcee03f7a3b7a04bf89fef740d7dd06b
Wyimaginowany <goralczyk.jakub@wp.pl>
2021-09-06 12:34:32 +0200

    files and layers organization

--- GameJam 2021.2/Assets/Scripts/Player/PlayerShooting.cs
+++ GameJam 2021.2/Assets/Scripts/Player/PlayerShooting.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
+
     [SerializeField] Weapon weapon;
     [SerializeField] Transform hand;
     [SerializeField] Transform firePoint;
+
+    private float damageUpgrade = 0;
+    private float fireRateUpgrade = 0;
 
     float lastFired = 0f;
 
@@ -15,17 +19,26 @@ public class PlayerShooting : MonoBehaviour
         weapon.SpawnWeapon(hand);
     }
 
-
     public void Shoot()
     {
-        if (Time.time - lastFired > 1 / weapon.fireRate)
+        if (Time.time - lastFired > 1 / weapon.fireRate + fireRateUpgrade)
         {
             lastFired = Time.time;
             GameObject bullet = Instantiate(weapon.bulletPrefab, firePoint.transform.position, firePoint.transform.rotation);
-            bullet.GetComponent<Bullet>().SetBulletDamage(weapon.damage);
+            bullet.GetComponent<Bullet>().SetBulletDamage(weapon.damage + damageUpgrade);
             Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
             bulletRigidbody.AddForce(firePoint.transform.up * weapon.bulletSpeed, ForceMode2D.Impulse);
         }     
+    }
+
+    public void UpgradeDamage(float value)
+    {
+        damageUpgrade += value;
+    }
+
+    public void UpgradeFireRate(float value)
+    {
+        fireRateUpgrade += value;
     }
 
 }
