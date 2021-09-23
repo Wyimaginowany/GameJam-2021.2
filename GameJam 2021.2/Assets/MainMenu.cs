cf33d11de5f356464008f7e1cc7dbe239656417f
Wyimaginowany <goralczyk.jakub@wp.pl>
2021-09-23 10:31:07 +0200

    main menu options
    sound and music volume, fullscreen toggle

---
+++ GameJam 2021.2/Assets/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+
+    public void SetSoundsVolume(float volume)
+    {
+        audioMixer.SetFloat("sounds", volume);
+    }
+
+    public void ShowCredits()
+    {
+        //credits UI set active true
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        //load scene (levelIndex)
+    }
+
+
+}
