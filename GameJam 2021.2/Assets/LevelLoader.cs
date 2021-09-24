170603b659e9a36af0a45d18c498f6b78002a30c
Wyimaginowany <goralczyk.jakub@wp.pl>
2021-09-24 15:43:17 +0200

    scene transition

---
+++ GameJam 2021.2/Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] float transitionTime = 2f;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadClickedLevel(int levelIndex)
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        animator.SetTrigger("start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
